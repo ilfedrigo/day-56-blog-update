155eae7f10c96fad4d84d0c98370aa884750612d
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-03-05 17:48:13 -0300

    All links and post previews working

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 import requests
 
+posts = requests.get("https://api.npoint.io/7dbf8679b0a5575ae1e6").json()
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts)
 
 @app.route('/about')
 def about():
@@ -15,6 +17,15 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
